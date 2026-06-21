011a95f51028514d1fb9f3206b20b180cc0e53d1
Hamzaafzaal786 <hamzaafzaal669@gmail.com>
2026-06-21 23:17:29 +0500

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -9,9 +9,9 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyDbHk2tBqTa3sCsZq_fLBDHa8ru7r0E86w',           // Replace this
-    appId: '1:1065123572322:android:d8fdd0c756be8fe82eff3b',             // Replace this
-    messagingSenderId: '1065123572322',      // Replace this
+    apiKey: 'APIkey*****************',           // Replace this
+    appId: 'AppID************************',             // Replace this
+    messagingSenderId: 'MessagingsenderID*******************',      // Replace this
     projectId: 'hamza-weatherapp',             // Replace this
     storageBucket: 'hamza-weatherapp.firebasestorage.app',
   );
